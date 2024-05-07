c702bc1ba1a905bd90898cb8bb4747427c7664d5
Jose Miguel Dueñas Torres <jomiguel097@gmail.com>
2024-05-07 06:03:45 -0500

    :hammer: Angular tools exploration, examples and configuration

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
@@ -11,9 +11,37 @@ import { CommonModule } from '@angular/common';
 })
 export class LabsComponent {
   welcome = 'Hola!';
-  tasks = [
+  tasks = signal([
     'Instalar Angular CLI',
     'Crear proyecto',
-    'Crear componentes'
-  ]
+    'Crear componentes',
+    'Crear servicio'
+  ]);
+  name = signal('José Miguel');
+  age = 26;
+  disabled = true;
+  img = 'https://w3schools.com/howto/img_avatar.png';
+
+  person = {
+    name : 'Miguel',
+    age : 26,
+    avatar : 'https://w3schools.com/howto/img_avatar.png'
+  }
+
+  clickHandler(){
+    alert('Efectivamente, diste click');
+  }
+
+  changeHandler(event: Event){
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    // Modificación del valor, es la mejor forma de hacer modificaciones
+    this.name.set(newValue);
+  }
+
+  keydownHandler(event: KeyboardEvent){
+    const input = event.target as HTMLInputElement;
+
+    console.log(input.value);
+  }
 }
